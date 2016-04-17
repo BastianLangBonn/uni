f9562f3a72843e0b39921cae41a40f22ae833d77
bastianlang@t-online.de <bastianlang@t-online.de>
2016-04-17 23:28:58 +0200

    added csv and started script

---
+++ assignment02/plot.py
@@ -0,0 +1,7 @@
+import matplotlib
+import matplotlib.cbook as cbook
+from matplotlib import pyplot as plt
+import numpy as np
+
+np.genfromtxt('SEE_Experiment1', delimiter=',', skip_header=2)
+print('foo')
