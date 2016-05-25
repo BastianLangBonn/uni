08f3a84ce56d6f37d57c805b52c39203272bace9
Bastian <bastianlang@t-online.de>
2016-05-25 17:37:58 +0200

    added program to load image files

---
+++ character_recognition/Untitled.m
@@ -0,0 +1,14 @@
+%% Read data
+clear;
+images = importdata('train.csv', ',', 1);
+for iImage = 1:length(images.data)
+    imageOne = images.data(iImage,:);
+    for i = 1:28
+       I{iImage}(i,:) = imageOne((i-1)*28+2:i*28+1);
+       Label{iImage} = imageOne(1);
+    end
+    %image(images(100:120,100:120))
+    image(I{iImage})
+    title(Label{iImage})
+    pause(1)
+end
