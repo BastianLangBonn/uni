c7d099850f83b00d1f2f5779f46553f6f2841d85
Bastian <bastianlang@t-online.de>
2016-06-13 09:23:26 +0200

    fixed bugs with tournament selection

--- esp/learnPolesWithEsp.m
+++ esp/learnPolesWithEsp.m
@@ -2,7 +2,7 @@ clear;
 addpath('simulator');
 parameters.topology = [6,2,1];
 evaluationFunction = 'twoPoleEvaluation';
-parameters.permutationOption = 2;
+parameters.permutationOption = 1;
 parameters.elitismOption = 2;
 parameters.maximumGenerations = 2500;
 parameters.targetFitness = 1000;
