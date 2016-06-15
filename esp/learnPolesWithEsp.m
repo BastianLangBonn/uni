668900fdcefcf790398d2b58b727638fc9022d5e
Bastian <bastianlang@t-online.de>
2016-06-15 20:42:49 +0200

    dont know

--- esp/learnPolesWithEsp.m
+++ esp/learnPolesWithEsp.m
@@ -2,12 +2,14 @@ clear;
 addpath('simulator');
 parameters.topology = [6,2,1];
 evaluationFunction = 'twoPoleEvaluation';
-parameters.permutationOption = 1;
+parameters.permutationOption = 2;
 parameters.elitismOption = 2;
 parameters.maximumGenerations = 2500;
 parameters.targetFitness = 1000;
 
 
 r = doEsp(evaluationFunction, parameters);
+
+% feval('twoPole_test',r.bestNetwork, @RNNet, parameters.targetFitness,'vis');
     
 
