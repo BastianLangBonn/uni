38ccca4a4f70b06a49977be93ff0e6c110c377ea
Bastian <bastianlang@t-online.de>
2016-07-13 17:03:22 +0200

    added bash script

---
+++ matlab/cluster_scripts/neat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PBS -q hpc
+#PBS -l nodes=1:ppn=32
+#PBS -l walltime=72:00:00
+#PBS -l vmem=100gb
+
+# change to submit directory (with executable)
+cd $PBS_O_WORKDIR/../experiments
+
+export MALLOC_ARENA_MAX=4
+
+# Load Module
+module load matlab/R2016a
+
+# Run experiment
+matlab -nodisplay -nosplash -nodesktop -r "optimalControl"
