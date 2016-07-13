6c3a192ffb861160094c2e3da7c23bde8a805dcf
Bastian <bastianlang@t-online.de>
2016-07-13 17:08:27 +0200

    added script with lower resources

---
+++ matlab/cluster_scripts/opt_control_fast.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PBS -q hpc2
+#PBS -l nodes=1:ppn=32
+#PBS -l walltime=1:00:00
+#PBS -l vmem=32gb
+
+# change to submit directory (with executable)
+cd $PBS_O_WORKDIR/../experiments
+
+export MALLOC_ARENA_MAX=4
+
+# Load Module
+module load matlab/R2016a
+
+# Run experiment
+matlab -nodisplay -nosplash -nodesktop -r "optimalControl"
