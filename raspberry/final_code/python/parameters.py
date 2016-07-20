49d576341c622f933dd8cc384c9132aff9a3f329
Bastian <bastianlang@t-online.de>
2016-07-20 15:14:29 +0200

    made python tests run

--- raspberry/final_code/python/parameters.py
+++ raspberry/final_code/python/parameters.py
@@ -1,11 +1,11 @@
 # GPIO PINs
-GPIO_BRAKE = 12
-GPIO_BUTTON = 11
-GPIO_PWM = 3
+GPIO_BRAKE = 18 # Board Pin 12
+GPIO_BUTTON = 17 # Board Pin 11
+GPIO_PWM = 2 # Board Pin 3
 
-PWM_STEP = 5.0 # percent
-PWM_MINIMUM = 0.0
-PWM_MAXIMUM = 100.0
+PWM_STEP = 0.1 # percent
+PWM_MINIMUM = 4.0 # -> first movement starts at 4.8
+PWM_MAXIMUM = 10.0
 PWM_FREQUENCY = 10.0 # Hz
 
 DELAY_BUTTON = 0.2 
--- raspberry/final_code/python/pwmTest.py
+++ raspberry/final_code/python/pwmTest.py
@@ -1,7 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+import parameters as p
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3,GPIO.OUT)
-p = GPIO.PWM(3,10.0)
-p.start(50.0)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+pin = GPIO.PWM(2,50)
+time.sleep(3)
+pin.start(1)
+for i in range(100):
+    pin.ChangeDutyCycle(i)
+    time.sleep(1)
+pin.stop()
+GPIO.cleanup()
+    
