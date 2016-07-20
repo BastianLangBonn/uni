6cad4ed8b3f41e81724ae744cc768497c90eb1eb
Bastian <bastianlang@t-online.de>
2016-07-20 15:34:23 +0200

    added brake input to controller

--- raspberry/final_code/python/controller/Controller.py
+++ raspberry/final_code/python/controller/Controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import threading
 from Button import Button
+from Brake import Brake
 import parameters as p
 import time
 import RPi.GPIO as GPIO
@@ -10,23 +11,32 @@ class Controller:
         
         # initialize variables
         self.currentActivation = p.PWM_MINIMUM
+        self.isBrakeActivated = False
         self.activationLock = threading.Lock()
+        self.brakeLock = threading.Lock()
         self.threads = []
         
         # initialize GPIO PINS
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(p.GPIO_BUTTON, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.setup(p.GPIO_BRAKE, GPIO.IN)
         
         # initialize threads
         self.button = Button("Button", self)
-        self.button.setDaemon(True) # enable keyboard interrupt to end all threads
+        self.brake = Brake("Brake", self)
+        
+        # declarate threads as daemons for keyboard interrupt
+        self.button.setDaemon(True) 
+        self.brake.setDaemon(True)
         self.threads.append(self.button)
+        self.threads.append(self.brake)
         
         
         
     def start(self):
         # start threads
         self.button.start()
+        self.brake.start()
         
         # wait for threads to end
         while threading.active_count() > 0:
@@ -41,6 +51,18 @@ class Controller:
             self.currentActivation = p.PWM_MAXIMUM
         print "Activation set to {}".format(self.currentActivation)
         self.activationLock.release()
+        
+    def notifyBrakeDeactivation(self):
+        if self.isBrakeActivated:
+            self.brakeLock.acquire(1)
+            self.isBrakeActivated = False
+            self.brakeLock.release()
+        
+    def notifyBrakeActivation(self):
+        if not self.isBrakeActivated:
+            self.brakeLock.acquire(1)
+            self.isBrakeActivated = True
+            self.brakeLock.release()
         
         
     
