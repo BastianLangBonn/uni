03d7aae878e8f5c06d660d51a5dea1082e236488
Bastian <bastianlang@t-online.de>
2016-07-20 16:39:43 +0200

    added gps thread

--- raspberry/final_code/python/controller/Controller.py
+++ raspberry/final_code/python/controller/Controller.py
@@ -3,6 +3,7 @@ import threading
 from Button import Button
 from Brake import Brake
 from ant import Ant
+from Gps import Gps
 import parameters as p
 import time
 import RPi.GPIO as GPIO
@@ -16,9 +17,14 @@ class Controller:
         self.currentPower = 0.0
         self.currentSpeed = 0.0
         self.currentTorque = 0.0
+        self.currentLatitude = 0.0
+        self.currentLongitude = 0.0
+        self.currentAltitude = 0.0
+        
         self.activationLock = threading.Lock()
         self.brakeLock = threading.Lock()
         self.antLock = threading.Lock()
+        self.gpsLock = threading.Lock()
         self.threads = []
         
         # initialize GPIO PINS
@@ -30,14 +36,17 @@ class Controller:
         self.button = Button("Button", self)
         self.brake = Brake("Brake", self)
         self.ant = Ant("Ant", self)
+        self.gps = Gps("Gps", self)
         
         # declarate threads as daemons for keyboard interrupt
         self.button.setDaemon(True) 
         self.brake.setDaemon(True)
         self.ant.setDaemon(True)
+        self.gps.setDaemon(True)
         self.threads.append(self.button)
         self.threads.append(self.brake)
         self.threads.append(self.ant)
+        self.threads.append(self.gps)
         
         
     def start(self):
@@ -45,6 +54,7 @@ class Controller:
         self.button.start()
         self.brake.start()
         self.ant.start()
+        self.gps.start()
         
         # wait for threads to end
         while threading.active_count() > 0:
@@ -73,7 +83,6 @@ class Controller:
             self.brakeLock.release()
             
     def notifySpeedUpdate(self, rpm):
-        print 'Speed Value Received'
         self.antLock.acquire(1)
         self.currentSpeed = rpm * p.WHEEL_LENGTH * 0.06
         print "Speed set to {}".format(self.currentSpeed)
@@ -88,12 +97,18 @@ class Controller:
         
     
     def notifyTorqueUpdate(self, torque):
-        print 'Speed Value Received'
         self.antLock.acquire(1)
         self.currentTorque = torque
         print "Torque set to {}".format(self.currentTorque)
         self.antLock.release()
         
+    def notifyGpsUpdate(self, latitude, longitude, altitude):
+        self.gpsLock.acquire(1)
+        self.currentLatitude = latitude
+        self.currentLongitude = longitude
+        self.currentAltitude = altitude
+        print "Set gps data to {},{},{}".format(self.currentLatitude, self.currentLongitude, self.currentAltitude)
+        self.gpsLock.release()
         
     
 controller = Controller()
